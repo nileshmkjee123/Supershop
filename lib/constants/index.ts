d43f6976d9353bbc7bbc17144fe938e182ad54ad
nilesh65 <nileshmukherjee65@gmail.com>
2025-08-23 14:45:30 +0530

    tailwind-animate-added

---
+++ lib/constants/index.ts
@@ -0,0 +1,3 @@
+export const APP_NAME= process.env.NEXT_PUBLIC_APP_NAME || 'Prostore'
+export const APP_DESCRIPTION = process.env.NEXT_PUBLIC_APP_DESCRIPTION || 'A modern e-commerce platform built with Next.js'
+export const SERVER_URL  = process.env.SERVER_URL || 'localhost:3000'
